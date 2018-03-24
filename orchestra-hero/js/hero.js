63c794218b97a45f997e9430dab0dc54208592fe
gregmeldrum@gmail.com <greg.meldrum@solace.com>
2018-03-24 19:26:37 -0400

    Add simple midi file generator

--- orchestra-hero/js/hero.js
+++ orchestra-hero/js/hero.js
@@ -1,3 +1,44 @@
+// var factoryProps = new solace.SolclientFactoryProperties();
+// factoryProps.profile = solace.SolclientFactoryProfiles.version10;
+// solace.SolclientFactory.init(factoryProps);
+
+// hosturl="<Solace Cloud Web Messaging URL and Port";
+// vpn="<VPN>";
+// username="<USER>";
+// pass="<PASS>";
+
+// // create session
+// session = solace.SolclientFactory.createSession({
+//     url:      hosturl,
+//     vpnName:  vpn,
+//     userName: username,
+//     password: pass
+// });
+
+// session.on(solace.SessionEventCode.MESSAGE, function (message) {
+//   let contents = message.getBinaryAttachment();
+//   let pianoKey = document.getElementById(contents);
+//   pianoKey.style.background = "red";
+//   setTimeout(function(){
+//       pianoKey.style.background = "white";
+//   }, 100);
+// });
+
+// session.on(solace.SessionEventCode.UP_NOTICE, function (sessionEvent) {
+//   console.log('Connected');
+// });
+
+// session.on(solace.SessionEventCode.SUBSCRIPTION_OK, function (sessionEvent) {
+//  console.log('Subscribed');
+// });
+
+// try {
+//   session.connect();
+// } catch (error) {
+//   console.log(error.toString());
+// }
+
+
 function setup() {
   mainLoop();
 }
