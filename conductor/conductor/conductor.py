36cfef6d9169e6f1065d86d8d041798bc67fea2e
gregmeldrum@gmail.com <greg.meldrum@solace.com>
2018-03-25 21:49:13 -0400

    Adding more smarts to the conductor. It can figure out how many unique notes are present

--- conductor/conductor/conductor.py
+++ conductor/conductor/conductor.py
@@ -1,31 +1,69 @@
+# Conductor
 import sys
 import mido
 
 from solace.client import SolaceMQTTClient
 
-# The conversion of MIDI notes is based on this: https://www.midikits.net/midi_analyser/midi_note_numbers_for_octaves.htm
-notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
+def get_unique_notes_in_channel(notes_in_channel):
+    """ Get an orderede set of unique notes in the channel """
+    all_notes = []
+    for notes in notes_in_channel:
+        all_notes.append(notes.note)
 
+    return sorted(set(all_notes))
+
+def analyze_song(mid):
+    """ Determine which tracks have enough notes to make it interesting """
+    for channel_id in range(len(mid.tracks)):
+        channel = mid.tracks[channel_id]
+        notes_in_channel = [n for n in channel if n.type == "note_on"]
+
+        if notes_in_channel:
+            channel_number = notes_in_channel[0].channel
+            channels[channel_number] = {
+                'instrument': channel.name.strip(),
+                'notes': len(notes_in_channel)
+            }
+            unique_notes = get_unique_notes_in_channel(notes_in_channel)
+            channels[channel_number]['unique'] = unique_notes
+
+def play_song(mid):
+    for msg in mid.play():
+        if msg.type == "note_on":
+            channel_number = msg.channel
+            unique_notes = channels[channel_number]['unique']
+            print(str(msg.channel) + ": " + notes[msg.note % 12])
+
+            # Message body
+            #  track: The track on the game controller that the note will be placed on (1..7)
+            #  note: The midi note number
+            #  channel: The midi channel that denotes the instrument
+            message_body = '{ ' + \
+            'track: ' + str(unique_notes.index(msg.note) + 1) + ',' +\
+            'note: ' + str(msg.note) + ',' + \
+            'channel: ' + str(channel_number) + '}'
+            print(message_body)
+            solace.publish("orchestra/" + theatre + '/' + str(msg.channel), 
+            message_body)
+
+# Topic format = orchestra/theatre/channel
+# orchestra - constant
+# theatre - the "room" that the song is played in. Default value is 'default'
+# channel - the "instument being played"
+theatre = 'default'
+
+# The conversion of MIDI notes is based on this: 
+# https://www.midikits.net/midi_analyser/midi_note_numbers_for_octaves.htm
+notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
 midi_file = sys.argv[1]
 
 mid = mido.MidiFile(midi_file)
 
 channels = {}
 
-# Determine which tracks have enough notes to make it interesting
-for track_id in range(len(mid.tracks)):
-    track = mid.tracks[track_id]
-    notes_in_track = [n for n in track if n.type == "note_on"]
-
-    if notes_in_track:
-        channels[notes_in_track[0].channel] = {
-            'instrument': track.name.strip(),
-            'notes': len(notes_in_track)
-        }
 
 solace = SolaceMQTTClient()
 
-for msg in mid.play():
-    if msg.type == "note_on":
-        print(str(msg.channel) + ": " + notes[msg.note % 12])
-        solace.publish("orchestra/" + str(msg.channel), notes[msg.note % 12])
+analyze_song(mid)
+play_song(mid)
+    
