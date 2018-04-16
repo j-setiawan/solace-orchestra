bda8a7e429cd2b4bc944029166183a860d2e8756
gregmeldrum@gmail.com <greg.meldrum@solace.com>
2018-04-15 21:44:21 -0400

    Webpackify the musician ui

--- orchestra-hero/lib/credentials.js
+++ orchestra-hero/lib/credentials.js
@@ -2,3 +2,5 @@ var hosturl = "ws://mr-91b692du2x.messaging.solace.cloud:20643";
 var vpn = "msgvpn-91b69332x9";
 var username = "solace-cloud-client";
 var pass = "rt9kruk1ul48fqe6e1fvobuauu";
+
+export {hosturl, vpn, username, pass};
