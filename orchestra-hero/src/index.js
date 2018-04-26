2be1f0452653489b65fbd06b98c5c4c5eb202321
gregmeldrum@gmail.com <greg.meldrum@solace.com>
2018-04-26 17:27:59 -0400

    Add score

--- orchestra-hero/src/index.js
+++ orchestra-hero/src/index.js
@@ -14,7 +14,7 @@ var messaging;
 var timeRef;
 var syncReady = false;
 var musicianName = '';
-
+var score = 0;
 // Amount of time it takes the slider to slide down the track
 var sliderTimeSecs = 1.5;
 var publisher = {};
@@ -199,6 +199,7 @@ function buildSlider(id, track, message) {
   slider.track = track;
   slider.id = id;
   slider.message = message;
+  slider.pressed = false;
   slider.element.className +=
     "slider slider-anim-" + track + " track" + track + " shape color" + track;
 
@@ -217,6 +218,10 @@ function addSlider(id, track, message) {
   setTimeout(function() {
     slider.element.remove();
     slider.removeTime = Date.now();
+    if (slider.pressed === false){
+      score -=10;
+      updateScore();
+    }
     slider = {};
   }, sliderTimeSecs * 1000);
 
@@ -294,6 +299,10 @@ function buttonPress(track) {
 
     publishSpontaneousNoteMessage(spontaneousNote);
   }
+}
+
+function updateScore() {
+   $("#score").html(score.toString());
 }
 
 function uuid() {
