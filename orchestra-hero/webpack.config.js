ce7e99f35cebbe34c9ef730d99831b450d468721
gregmeldrum@gmail.com <greg.meldrum@solace.com>
2018-05-03 08:58:29 -0400

    Change background

--- orchestra-hero/webpack.config.js
+++ orchestra-hero/webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|gif)$/,
-                loader: "file-loader?name=/assets/[name].[ext]"
+                loader: "url-loader?name=/assets/[name].[ext]"
             },
             {
                 test: /\.(wav|woff|woff2|eot|ttf|otf)$/,
