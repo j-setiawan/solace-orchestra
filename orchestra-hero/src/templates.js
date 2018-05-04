969d8f577077280978ebb222cca629ad9e8d0e9d
gregmeldrum@gmail.com <greg.meldrum@solace.com>
2018-05-04 12:46:33 -0400

    UI improvements

--- orchestra-hero/src/templates.js
+++ orchestra-hero/src/templates.js
@@ -14,69 +14,68 @@ let formatters = {
 
 export default templates = {
 
-  page:   (score, number, instrumentInfo) =>
+  page: (score, number, instrumentInfo) =>
     $div(
       templates.dialog(),
       $div(
-        $img({cn: "heading", height: "48px", width: "225px", src: "assets/solaceSymphonyInverted.png"}),
+        { cn: "outermost" },
         $div(
-          {cn: "outermost"},
+          { cn: "outer1" },
           $div(
-            {cn: "outer1"},
-            $div(
-              {id: "lines"},
-              [...Array(7).keys()].map(num => $div({cn: `line line${num+1}`}))
-            ),
-            $div(
-              {id: "sliders"}
-            ),
-            $div(
-              {id: "countdown"},
-              jst.stamp("number", templates.countdown, number)  
-            )
-          ),
-          
-          $div(
-            {id: "buttons", cn: "hide"},
-            [...Array(7).keys()].map(num => $button({cn: `my-button button${num+1} color${num+1}`, id: `button${num+1}`}))
+            { id: "lines" },
+            [...Array(7).keys()].map(num => $div({ cn: `line line${num + 1}` }))
           ),
           $div(
-            {id: "score"},
-            jst.stamp("score", templates.scoreTable, score) 
+            { id: "sliders" }
+          ),
+          $div(
+            { id: "countdown" },
+            jst.stamp("number", templates.countdown, number)
           )
+        ),
+
+        $div(
+          { id: "buttons", cn: "hide" },
+          [...Array(7).keys()].map(num => $button({ cn: `my-button button${num + 1} color${num + 1}`, id: `button${num + 1}` }))
+        ),
+        $div(
+          { id: "score" },
+          jst.stamp("score", templates.scoreTable, score)
+        ),
+        $div(
+          { id: "instruments" },
+          jst.stamp("instruments", templates.instrument, instrumentInfo)
+        ),
+        $div(
+          $img({ cn: "heading", height: "48px", width: "225px", src: "assets/solaceSymphonyInverted.png" }),
         )
-      ),
-      $div(
-        {id: "instruments"},
-        jst.stamp("instruments", templates.instrument, instrumentInfo) 
       )
     ),
 
-  dialog:  () =>
+  dialog: () =>
     $div(
-      {cn:"modal fade", "data-backdrop":"static", "data-keyboard":"false", id:"getNameModal", tabindex:"-1", role:"dialog", "aria-labelledby":"exampleModalLabel", "aria-hidden":"true"},
+      { cn: "modal fade", "data-backdrop": "static", "data-keyboard": "false", id: "getNameModal", tabindex: "-1", role: "dialog", "aria-labelledby": "exampleModalLabel", "aria-hidden": "true" },
       $div(
-        {cn:"modal-dialog modal-sm", role:"document"},
+        { cn: "modal-dialog modal-sm", role: "document" },
         $div(
-          {cn:"modal-content"},
+          { cn: "modal-content" },
           $div(
-            {cn:"modal-body"},
+            { cn: "modal-body" },
             $div(
-              {cn:"form-group"},
+              { cn: "form-group" },
               $label(
-                {for:"recipient-name", cn:"col-form-label"},
+                { for: "recipient-name", cn: "col-form-label" },
                 "Name:"
               ),
               $input(
-                {type:"text", cn:"form-control", id:"musician-name"}
+                { type: "text", cn: "form-control", id: "musician-name" }
               )
-            )        
-          )        
-        )        
-        
-      )   
+            )
+          )
+        )
+      )
     ),
-  
+
   countdown: (number) =>
     $div(
       number
@@ -100,22 +99,24 @@ export default templates = {
 
   instrument: (instrumentInfo) =>
     $div(
-      {cn: "instrument-div"},
+      { cn: "instrument-div" },
       $div("Current Instrument"),
       $div(
-        {cn: "instrument-select"},
-        instrumentInfo.selectOn ? 
+        { cn: "instrument-select" },
+        instrumentInfo.selectOn ?
           $select(
-            {events: instrumentInfo.events.selectOn},
+            { events: instrumentInfo.events.selectOn },
             instrumentInfo.list.map(entry => $option(
-              {value: entry.index,
-               properties: entry.selected ? ['selected'] : []},
+              {
+                value: entry.index,
+                properties: entry.selected ? ['selected'] : []
+              },
               entry.name))
           )
-        : $div(
-          {events: instrumentInfo.events.selectOff},
-          instrumentInfo.list[instrumentInfo.currentInstrument].name
-        )
+          : $div(
+            { events: instrumentInfo.events.selectOff },
+            instrumentInfo.list[instrumentInfo.currentInstrument].name
+          )
       )
     )
 
